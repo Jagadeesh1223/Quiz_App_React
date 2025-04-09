6c225a05ee89a3261deb0bb32a43378039d0b2ac
Jagadeesh1223 <jagadeeshmantri23@gmail.com>
2025-04-09 18:40:49 +0530

    Quiz App commit

---
+++ src/QuizApp.jsx
@@ -0,0 +1,91 @@
+import React from "react";
+import {data} from './data';
+import { useState } from "react";
+import'./style.css';
+export default function QuizApp() {
+    const [index, setIndex] = useState(0);
+    const [score, setScore] = useState(0);
+    const [quizFinished, setQuizFinished] = useState(false);
+    const [quizStart, setQuizStart] = useState(false);
+    const [option, setOption] = useState(null);
+
+    // These below options are correct all Questions by order first Question to last Question.
+    const correctAns = ['option2'// 1st Question Ans
+        ,'option3' // 2nd Question Ans
+        ,'option2', // 3rd Question Ans
+        'option2','option3',
+        'option2','option2','option3','option2','option2','option2','option3',
+        'option4','option3','option2','option2','option1','option3','option3',
+        'option2'];
+
+    const handleStart = (start) => {
+        setQuizStart(start); // it is handle the start button
+    }
+    const handleSelect = (selectedopt) => {
+        setOption(selectedopt);
+    }
+    // it is uesed to handle the NEXT button by moving next Question
+    const handleNext = () => {
+        if(option === correctAns[index]) {
+            setScore(score+1);
+        }
+        if(index<data.length-1) {
+            setIndex(index+1);
+            setOption(null);
+        } else {
+            setQuizFinished(true);
+        }
+    }
+    // After click on the submit button you get below window
+    if(quizFinished) {
+        return (
+            <div className="quiz">
+                <h1>QuizFinished..!</h1>
+                <h3>Your Score is : {score} out of {data.length}</h3>
+            </div>
+        )
+    }
+    // Before go to the Quiz. 
+    if(!quizStart) {
+        return (
+            <div className="quiz">
+                <h1>Let's Start the Quiz</h1>
+                <h3>Total Number Questions : {data.length}</h3>
+                <button onClick= {() => handleStart(true)}>Start</button>
+            </div>
+        )
+    }
+     // it is uesed to handle the Previous button by moving previous Question
+    const handlePrev = () => {
+        if(index>0) 
+        setIndex(index-1);
+    }
+    // When click on the Start button the return function is get
+    if(quizStart) {
+        
+    return (
+        
+        <div className="quiz">
+            
+            <h2>Quiz App</h2>
+            
+            <h3>{index+1} ) {data[index].Queston}</h3>
+            <ul>
+                <li className={ option === 'option1' ? 'selected': '' }
+                    onClick={()=>handleSelect('option1')}>{data[index].option1}</li>
+                <li className={ option === 'option2' ? 'selected' : '' }
+                    onClick={()=>handleSelect('option2')}>{data[index].option2}</li>
+                <li className={ option === 'option3' ? 'selected' : '' }
+                    onClick={()=>handleSelect('option3')}>{data[index].option3}</li>
+                <li className={ option === 'option4' ? 'selected' : '' }
+                    onClick={()=>handleSelect('option4')}>{data[index].option4}</li>
+            </ul>
+            <div>
+            {index === 0 ? "" : <button onClick={handlePrev}>Previous</button>}
+            <button onClick={handleNext} disabled = {!option} className = "buttonnext">{index === data.length-1 ? "Submit" : "Next"}</button>
+            </div>
+            <h5>Question {index+1} of {data.length}</h5>
+        </div> 
+    );
+    }
+}
---
+++ src/data.js
@@ -0,0 +1,145 @@
+
+export const data = [
+    {
+        Queston: 'What is React?',
+        option1: 'It is library of JS',
+        option2: 'It is Markup Language',
+        option3: 'It is Objected Language',
+        option4: 'None'
+    },
+    {
+        Queston: 'What is the capital of France?',
+        option1: ' Madrid',
+        option2: 'Berlin',
+        option3: 'Paris',
+        option4: 'Rome'
+    },
+    {
+        Queston: 'Which element has the chemical symbol "O"?',
+        option1: 'Gold',
+        option2: 'Oxygen',
+        option3: 'Osmium',
+        option4: 'Ozone'
+    },
+    {
+        Queston: 'What is the largest planet in our solar system?',
+        option1: 'Earth',
+        option2: 'Jupiter',
+        option3: 'Mars',
+        option4: 'Venus'
+    },
+    {
+        Queston: 'Who wrote the play "Romeo and Juliet"?',
+        option1: 'Charles Dickens',
+        option2: 'Mark Twain',
+        option3: 'William Shakespeare',
+        option4: 'Jane Austen'
+    },
+    {
+        Queston: 'What is the tallest mountain in the world?',
+        option1: 'Mount Kilimanjaro',
+        option2: 'Mount Everest',
+        option3: 'K2',
+        option4: 'Mount Fuji'
+    },
+    {
+        Queston: 'Which country is known as the Land of the Rising Sun?',
+        option1: 'China',
+        option2: 'Japan',
+        option3: 'India',
+        option4: 'Thailand'
+    },
+
+    {
+        Queston: 'Which famous scientist developed the theory of general relativity?',
+        option1: 'Isaac Newton',
+        option2: 'Nikola Tesla',
+        option3: 'Albert Einstein',
+        option4: 'Galileo Galilei'
+    },
+    {
+        Queston: 'Which is the longest river in the world?',
+        option1: 'Amazon',
+        option2: 'Nile',
+        option3: 'Yangtze',
+        option4: 'Mississippi'
+    },{
+        Queston: 'What is the main ingredient in guacamole?',
+        option1: 'Tomatoes',
+        option2: 'Avocados',
+        option3: 'Peppers',
+        option4: 'Lemons'
+    },
+    {
+        Queston: 'Who painted the Mona Lisa?',
+        option1: 'Michelangelo',
+        option2: 'Leonardo da Vinci',
+        option3: 'Pablo Picasso',
+        option4: 'Vincent van Gogh'
+    },
+    {
+        Queston: 'What is the square root of 144?',
+        option1: '10',
+        option2: '11',
+        option3: '12',
+        option4: '14'
+    },
+    {
+        Queston: 'Which of these is not a continent?',
+        option1: 'Africa',
+        option2: 'Asia',
+        option3: 'Antarctica',
+        option4: 'Greenland'
+    },
+    {
+        Queston: 'What is the largest organ in the human body?',
+        option1: 'Brain',
+        option2: 'Heart',
+        option3: 'Skin',
+        option4: 'Liver'
+    },
+    {
+        Queston: 'Which bird is known for its colorful tail feathers and ability to mimic sounds?',
+        option1: 'Sparrow',
+        option2: 'Parrot',
+        option3: 'Eagle',
+        option4: 'Penguin'
+    },
+    {
+        Queston: 'What is the boiling point of water in Celsius?',
+        option1: '50°C',
+        option2: '100°C',
+        option3: '150°C',
+        option4: '200°C'
+    },
+    {
+        Queston: 'Which country is home to the Great Barrier Reef?',
+        option1: 'Australia',
+        option2: 'New Zealand',
+        option3: 'United States',
+        option4: 'Brazil'
+    },
+    {
+        Queston: 'Which gas do plants primarily use for photosynthesis?',
+        option1: 'Oxygen',
+        option2: 'Nitrogen',
+        option3: 'Carbon dioxide',
+        option4: 'Hydrogen'
+    },
+    {
+        Queston: 'What is the currency used in Japan?`',
+        option1: 'Dollar',
+        option2: 'Euro',
+        option3: 'Yen',
+        option4: 'Pound'
+    },
+    {
+        Queston: 'In which city would you find the famous landmark, the Eiffel Tower?',
+        option1: 'London',
+        option2: 'Paris',
+        option3: 'Rome',
+        option4: 'New York'
+    }
+    
+    
+]
